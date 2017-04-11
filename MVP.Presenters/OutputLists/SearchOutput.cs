54d37aad1e4f88f7ac17621e494823f869d791e8
Оксана Джурик <oksi2196@outlook.com>
2017-04-11 20:29:57 +0300

    changed search

--- MVP.Presenters/OutputLists/SearchOutput.cs
+++ MVP.Presenters/OutputLists/SearchOutput.cs
@@ -11,42 +11,41 @@ namespace MVP.Presenters.OutputList
         {
             string result = "";
             SearchByAuthor search = new SearchByAuthor();
-            
-            List<PolygraphicItem> resultList = search.Search(enteredAuthor,list);
-            if(resultList.Count==0)
+            List<PolygraphicItem> resultList = search.Search(enteredAuthor, list);
+            if (resultList.Count == 0)
             {
                 return "No items!";
             }
             List<Book> books = new List<Book>();
             List<Magazine> magazines = new List<Magazine>();
             List<Newspaper> newspapers = new List<Newspaper>();
-                        resultList.ForEach(x =>
-            {
-                if(x is Book)
-                {
-                    books.Add(x as Book);                    
-                }
-                if(x is Magazine)
-                {
-                    magazines.Add(x as Magazine);
-                }
-                if(x is Newspaper)
-                {
-                    newspapers.Add(x as Newspaper);
-                }
-            } 
-            );
-                       if(books.Count!=0)
+            resultList.ForEach(x =>
+{
+    if (x is Book)
+    {
+        books.Add(x as Book);
+    }
+    if (x is Magazine)
+    {
+        magazines.Add(x as Magazine);
+    }
+    if (x is Newspaper)
+    {
+        newspapers.Add(x as Newspaper);
+    }
+}
+);
+            if (books.Count != 0)
             {
                 BookStringOutput resultOutput = new BookStringOutput();
                 result = resultOutput.ListOutput(books).ToString();
             }
-            if(magazines.Count!=0)
+            if (magazines.Count != 0)
             {
                 MagazineStringOutput resultOutput = new MagazineStringOutput();
                 result += resultOutput.ListOutput(magazines).ToString();
             }
-            if(newspapers.Count!=0)
+            if (newspapers.Count != 0)
             {
                 NewspaperStringOutput resultOutput = new NewspaperStringOutput();
                 result += resultOutput.ListOutput(newspapers).ToString();
